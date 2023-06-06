eed36e32bf53cca196b5d89f63a17d765c326b7e
YJ3657 <yeongjay2@gmail.com>
2023-06-06 22:44:30 +0900

    Added comments

--- app/src/main/java/com/example/unilink/MainActivity.java
+++ app/src/main/java/com/example/unilink/MainActivity.java
@@ -5,7 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
-    // progress being made
+    // check
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
